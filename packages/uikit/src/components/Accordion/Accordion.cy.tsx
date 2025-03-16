b3ec84c709fb35da2edfe7130c0db84360fb60fb
thavixt <komlosi.peti@gmail.com>
2025-03-16 20:44:07 +0100

    fix: accordion test

--- packages/uikit/src/components/Accordion/Accordion.cy.tsx
+++ packages/uikit/src/components/Accordion/Accordion.cy.tsx
@@ -7,16 +7,16 @@ const { Default } = composeStories(stories);
 describe('Accordion component', () => {
     it('should render', () => {
         mount(<Default />);
-        cy.get('[data-testid="content"]').should('not.be.visible');
+        cy.get('[data-testid="accordion-body"]').should('not.be.visible');
 
-        cy.get('[data-testid="title"]').should('be.visible');
-        cy.get('[data-testid="title-open"]').should('not.be.visible');
+        cy.get('[data-testid="accordion-title"]').should('be.visible');
+        cy.get('[data-testid="accordion-opentitle"]').should('not.be.visible');
 
-        cy.get('[data-testid="title"]').click();
+        cy.get('[data-testid="accordion-title"]').click();
 
-        cy.get('[data-testid="title"]').should('not.be.visible');
-        cy.get('[data-testid="title-open"]').should('be.visible');
+        cy.get('[data-testid="accordion-title"]').should('not.be.visible');
+        cy.get('[data-testid="accordion-opentitle"]').should('be.visible');
 
-        cy.get('[data-testid="content"]').should('be.visible');
+        cy.get('[data-testid="accordion-body"]').should('be.visible');
     });
 });
