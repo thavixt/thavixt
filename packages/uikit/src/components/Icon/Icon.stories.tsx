064abce172359803a1e52322138406b9867bb3fa
thavixt <komlosi.peti@gmail.com>
2025-04-17 14:34:11 +0200

    fix: Icon colors

--- packages/uikit/src/components/Icon/Icon.stories.tsx
+++ packages/uikit/src/components/Icon/Icon.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Icon } from '../Icon/Icon';
 import { ComponentProps } from 'react';
 import { IconList, IconType } from '../Icon/IconList';
+import { Typography } from '../Typography/Typography';
 
 const meta = {
   title: 'Illustration/Icon',
@@ -24,12 +25,12 @@ export const All: Story = {
   render: function StoryComponent(args: ComponentProps<typeof Icon>) {
     return (
       <>
-        <table className="table-auto border-collapse text-slate-800 dark:text-slate-100">
+        <table className="table-auto border-collapse">
           <tbody>
             {Object.keys(IconList).sort().map((key) => (
               <tr key={key}>
                 <td className='p-2 border border-slate-200 dark:border-slate-500'>
-                  <code>{key}</code>
+                  <Typography.Code>{key}</Typography.Code>
                 </td>
                 <td className='p-2 border border-slate-200 dark:border-slate-500'>
                   <Icon {...args} icon={key as IconType} />
