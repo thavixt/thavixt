6c4026fd6febbdb02d9e48afc904fa37a309533b
thavixt <komlosi.peti@gmail.com>
2025-04-24 15:04:36 +0200

    refactor: Progress component styles

--- packages/uikit/src/components/Progress/Progress.stories.tsx
+++ packages/uikit/src/components/Progress/Progress.stories.tsx
@@ -9,9 +9,11 @@ const meta = {
   component: Progress,
   tags: ['autodocs'],
   args: {
-    current: 15,
-    label: 'Downloading @thavixt/uikit from npm',
+    current: 95,
+    label: 'Progress:',
     max: 100,
+    height: '20px',
+    inline: false,
   },
 } satisfies Meta<typeof Progress>;
 
@@ -19,16 +21,16 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
-
 export const Example: Story = {
   args: {
+    current: 15,
     inline: true,
-    label: 'Progress',
+    label: 'Progress:',
   },
   render: function StoryComponent({ label, max = 100, ...args }: ComponentProps<typeof Progress>) {
     const [value, setValue] = useState(args.current);
     const [stopped, setStopped] = useState(true);
+
     useEffect(() => {
       if (stopped) {
         return;
@@ -43,12 +45,13 @@ export const Example: Story = {
       }), 250);
       return () => clearInterval(interval);
     }, [max, stopped]);
+
     return (
       <div className='flex space-x-8 align-center'>
         <ButtonBar>
-          <Button variant='silent' onClick={() => setValue(0)}>Reset</Button>
-          <Button variant={stopped ? 'primary' : 'danger'} onClick={() => setStopped(prev => !prev)}>
-            {stopped ? 'Start' : 'Stop'}
+          <Button title='Reset' variant='silent' onClick={() => setValue(0)}>&#8635;</Button>
+          <Button title={stopped ? 'Start' : 'Stop'} variant={stopped ? 'primary' : 'danger'} onClick={() => setStopped(prev => !prev)}>
+            {stopped ? <span>&#11122;</span> : <span>&#10539;</span>}
           </Button>
         </ButtonBar>
         <Progress {...args} label={label} max={max} current={value} />
@@ -57,30 +60,54 @@ export const Example: Story = {
   },
 };
 
-export const Colors: Story = {
-  render: () => (
+// export const Default: Story = {
+//   args: {
+//     label: 'Downloading @thavixt/uikit from npm:',
+//   },
+// };
+
+export const DefaultColors: Story = {
+  args: {
+    label: undefined,
+  },
+  render: StoryColorListComponent,
+};
+
+export const CustomColors: Story = {
+  args: {
+    label: undefined,
+    getColorFromValue(value, max) {
+      return `rgb(${200 - 200 * Math.fround(value/max)}, 0, ${200 * Math.fround(value/max)})`;
+    },
+  },
+  render: StoryColorListComponent,
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function StoryColorListComponent({ current, max, ...args }: ComponentProps<typeof Progress>) {
+  return (
     <div className='flex flex-col space-y-1'>
-      <Progress max={100} current={0} />
-      <Progress max={100} current={100 * 0.05} />
-      <Progress max={100} current={100 * 0.10} />
-      <Progress max={100} current={100 * 0.15} />
-      <Progress max={100} current={100 * 0.20} />
-      <Progress max={100} current={100 * 0.25} />
-      <Progress max={100} current={100 * 0.30} />
-      <Progress max={100} current={100 * 0.35} />
-      <Progress max={100} current={100 * 0.40} />
-      <Progress max={100} current={100 * 0.44} />
-      <Progress max={100} current={100 * 0.50} />
-      <Progress max={100} current={100 * 0.55} />
-      <Progress max={100} current={100 * 0.60} />
-      <Progress max={100} current={100 * 0.65} />
-      <Progress max={100} current={100 * 0.70} />
-      <Progress max={100} current={100 * 0.75} />
-      <Progress max={100} current={100 * 0.80} />
-      <Progress max={100} current={100 * 0.85} />
-      <Progress max={100} current={100 * 0.90} />
-      <Progress max={100} current={100 * 0.95} />
-      <Progress max={100} current={100} />
+      <Progress {...args} max={100} current={0} />
+      <Progress {...args} max={100} current={100 * 0.05} />
+      <Progress {...args} max={100} current={100 * 0.10} />
+      <Progress {...args} max={100} current={100 * 0.15} />
+      <Progress {...args} max={100} current={100 * 0.20} />
+      <Progress {...args} max={100} current={100 * 0.25} />
+      <Progress {...args} max={100} current={100 * 0.30} />
+      <Progress {...args} max={100} current={100 * 0.35} />
+      <Progress {...args} max={100} current={100 * 0.40} />
+      <Progress {...args} max={100} current={100 * 0.44} />
+      <Progress {...args} max={100} current={100 * 0.50} />
+      <Progress {...args} max={100} current={100 * 0.55} />
+      <Progress {...args} max={100} current={100 * 0.60} />
+      <Progress {...args} max={100} current={100 * 0.65} />
+      <Progress {...args} max={100} current={100 * 0.70} />
+      <Progress {...args} max={100} current={100 * 0.75} />
+      <Progress {...args} max={100} current={100 * 0.80} />
+      <Progress {...args} max={100} current={100 * 0.85} />
+      <Progress {...args} max={100} current={100 * 0.90} />
+      <Progress {...args} max={100} current={100 * 0.95} />
+      <Progress {...args} max={100} current={100} />
     </div>
   )
-};
+}
--- packages/uikit/src/components/Progress/Progress.tsx
+++ packages/uikit/src/components/Progress/Progress.tsx
@@ -1,26 +1,56 @@
 import classNames from "classnames";
 import { CommonProps } from "../../common/commonProps";
-import { useLayoutEffect, useRef, useState } from "react";
+import { JSX, useCallback, useLayoutEffect, useRef, useState } from "react";
 import { Typography } from "../Typography/Typography";
 import { useResizeObserver } from "../../common/useResizeObserver";
+import "./Progress.css";
 
 interface ProgressProps extends CommonProps<HTMLProgressElement> {
+  /** Current value */
   current: number;
+  /** Max value */
   max?: number;
+  /** Label text */
   label?: string;
+  /** Display label and bar inline */
   inline?: boolean;
+  /** Progress bar height */
+  height?: string;
+  /** Dynamically get a CSS color for the progress bar based on the current value */
+  getColorFromValue?: (value: number, max: number) => string;
 }
 
-export function Progress({ label, className, max = 100, current, inline, ...props }: ProgressProps) {
+/**
+ * A functional React component that renders a customizable progress bar with optional labels and styles.
+ *
+ * @example
+ * ```tsx
+ * <Progress current={47} label="Downloading archive" />
+ * ```
+ *
+ * @remarks
+ * - The component uses a `useResizeObserver` hook to detect size changes and adjust the label's position.
+ * - The progress percentage is calculated as `(current / max) * 100` and displayed as a label.
+ * - The label is conditionally rendered inside or outside the progress bar based on available space.
+ * - The `getRGBColorFromNumber` function is used to dynamically determine the bar's background color.
+ */
+export function Progress({
+  className,
+  current,
+  inline,
+  label,
+  getColorFromValue: providedGetColorFromValue,
+  height = '20px',
+  max = 100,
+  ...props
+}: ProgressProps): JSX.Element {
   const [resizeCounter, setResizeCounter] = useState(0);
   const [labelInside, setLabelInside] = useState(false);
   const progressRef = useRef<HTMLDivElement>(null);
-  useResizeObserver(() => setResizeCounter(prev => prev + 1));
-
-  const labelClasses = 'text-slate-600 dark:text-slate-200 text-xs';
-  const insideLabelClasses = 'text-slate-200 text-xs';
   const percent = +((current / max) * 100).toFixed();
-  const percentLabel = `${percent.toString().padStart(3)}%`;
+  const percentLabel = `${percent.toString().padStart(3)}% (${Math.round(current)}/${Math.round(max)})`;
+  
+  useResizeObserver(() => setResizeCounter(prev => prev + 1));
 
   useLayoutEffect(() => {
     if (!progressRef.current) {
@@ -30,28 +60,40 @@ export function Progress({ label, className, max = 100, current, inline, ...prop
     setLabelInside(labelRect.width > percentLabel.length * 10);
   }, [percent, percentLabel.length, resizeCounter]);
 
+  const getRGBColorFromNumber = useCallback((value: number, max: number) => {
+    if (providedGetColorFromValue) {
+      return providedGetColorFromValue(value, max);
+    }
+    return defaultGetColorFromValue(value, max);
+  }, [providedGetColorFromValue]);
+
   return (
-    <div className={classNames('grid space-y-2 items-center gap-x-2 gap-y-1 w-full', className, {
-      'grid-cols-1': !inline,
-      'grid-cols-[auto_minmax(100px,_1fr)]': inline
-    },
-    )}>
-      {label ? <Typography type="label" className="m-0">{label}:</Typography> : null}
-      <progress hidden max={max} value={current} {...props}>{percentLabel}</progress>
-      <div aria-hidden="true" className="rounded-full h-4 w-full bg-gray-200 dark:bg-gray-600 flex space-x-2">
+    <div className={classNames(
+      'Progress',
+      inline && 'Progress--inline',
+      className,
+    )}
+      // @ts-expect-error css variable declaration - types dont support it but works 
+      style={{ '--barHeight': height }}
+    >
+      {label ? (
+        <Typography type="label">{label}</Typography>
+      ) : null}
+      <progress hidden max={max} value={current} {...props}>
+        {percentLabel}
+      </progress>
+      <div aria-hidden="true" className="Progress__wrapper">
         <div
           ref={progressRef}
-          className="h-4 rounded-full flex justify-center transition-all"
+          className="Progress__bar"
           style={{ width: `${percent}%`, backgroundColor: getRGBColorFromNumber(percent, max) }}
         >
           {labelInside ? (
-            <div className={insideLabelClasses}>{percentLabel}</div>
+            <div className="Progress__label Progress--labelInside">{percentLabel}</div>
           ) : null}
         </div>
         {!labelInside ? (
-          <div className={labelClasses}>
-            {percentLabel}
-          </div>
+          <div className="Progress__label Progress--labelOutside">{percentLabel}</div>
         ) : null}
       </div>
     </div >
@@ -59,14 +101,18 @@ export function Progress({ label, className, max = 100, current, inline, ...prop
 }
 
 function getRatio(value: number, max: number) {
-  if (max === 0) return 0; // Avoid division by zero
-  return Math.min(100, Math.max(0, (value / max) * 100)); // Clamp between 0-100
+  if (max === 0) {
+    return 0;
+  }
+  return Math.min(100, Math.max(0, (value / max) * 100));
 }
 
-function getRGBColorFromNumber(value: number, max: number) {
+const TRANSITION_FACTOR = 2; // 1: linear, 100: instant at the end
+
+function defaultGetColorFromValue(value: number, max: number) {
   const clampedNum = getRatio(value, max);
-  const factor = Math.pow(clampedNum / 100, 2); // Exponential transition
-  const blue = Math.floor(200 * (1 - factor)); // Starts at 255 and drops faster near 100
-  const green = Math.floor(200 * factor); // Starts low and rises sharply near 100
-  return `rgb(0, ${green}, ${blue})`;
+  const transition = Math.pow(clampedNum / 100, TRANSITION_FACTOR); // Exponential transition
+  const blue = Math.floor(175 * (1 - transition)); // Starts at 255 and drops faster near 100
+  const green = Math.floor(175 * transition); // Starts low and rises sharply near 100
+  return `rgb(50, ${green}, ${blue})`;
 }
